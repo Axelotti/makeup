73e102fd5c9450d985146980d58d45a096f287fe
Axelotti <112974056+Axelotti@users.noreply.github.com>
2022-12-03 18:32:56 +0000

    changes

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask
+from flask import render_template, request
 
+app = Flask(__name__)
+
+def readDetails(filepath):
+    with open(filepath, 'r') as f:
+        return [line for line in f]
+
+@app.route("/")
+def homePage():
+    groupName= 'Group 7:'
+    name = 'Axel Cortes, Israel Lopez, Eric Villa, Erick Montenegro'
+    details = readDetails('static/details.txt')
+    return render_template('project.html', name = name, aboutMe = details, groupName = groupName)
